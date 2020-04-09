c5f1490f4d4a0f5fa282020375ddd9904031ff9f
MareK985 <59637752+MareK985@users.noreply.github.com>
2020-04-09 18:37:16 +0200

    svg logo stroke animation in hero

--- pages/case-study-expar.vue
+++ pages/case-study-expar.vue
@@ -8,6 +8,8 @@
       </header>
     </div>
     <div class="cha-text mt-2">
+      <Simple/>
+      <Stagger/>
       Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
     </div>
   </section>
@@ -30,12 +32,29 @@
       </header>
     </div>
     <div class="cha-text">
+      <Simple/>
       There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc
     </div>
   </section>
 </main>
     </div>
 </template>
+
+<script>
+
+import Simple from '~/components/Simple.vue'
+import Stagger from '~/components/Stagger.vue'
+
+
+export default {
+  components: {
+    Simple,
+    Stagger
+  }
+  
+}
+</script>
+
 
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900&display=swap');
