968ee2bb9aa4a04c1078bdc8fe1ed0a64d53f709
MareK985 <59637752+MareK985@users.noreply.github.com>
2020-04-09 21:17:30 +0200

    Intersection Observer - setup
    - issue with the code - document not found????

--- pages/case-study-expar.vue
+++ pages/case-study-expar.vue
@@ -1,55 +1,70 @@
 <template>
     <div>
-        <main>
-  <section>
-    <div class="cha-header-clip">
-      <header class="cha-header">
-        case study: ExpAR store
-      </header>
-    </div>
-    <div class="cha-text mt-2">
-      Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-    </div>
-  </section>
-  
-  <section class="content" data-theme="dark">
-    <div class="cha-header-clip" aria-hidden="true">
-      <header class="cha-header">
-        case study: ExpAR store
-      </header>
-    </div>
-    <div class="cha-text mt-2 left">
-      <img src="~/assets/img/path.jpg" width="400px" alt="image" class="image"/>
-      <p>The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-      </p>
-      <img src="~/assets/img/logo-1.svg" width="400px" alt="symbol" class="symbol"/>
-    </div>
-    <div class="cha-text mt-2 right">
-      <div>
-        <h2>Activity#1: Interviews</h2>
-        <p>T you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc
-    	  </p>
-      </div>
+      <main>
+        <section>
+          <div class="cha-header-clip">
+            <header class="cha-header">
+              case study: ExpAR store
+            </header>
+          </div>
+          <div class="cha-text mt-2">
+            Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+          </div>
+        </section>
+        
+        <section class="content" data-theme="dark">
+          <div class="cha-header-clip" aria-hidden="true">
+            <header class="cha-header">
+              case study: ExpAR store
+            </header>
+          </div>
+          <div class="cha-text mt-2 left">
+            <img src="~/assets/img/path.jpg" width="400px" alt="image" class="image amim" data-delay="0s"/>
+            <p class="anim" data-delay=".5s">Line in section 1.10.32.
+            </p>
+            <img src="~/assets/img/logo-1.svg" width="400px" alt="symbol" class="symbol anim" data-delay=".7s"/>
+          </div>
+          <div class="cha-text mt-2 right">
+            <div class="anim" data-delay="1s">
+              <h2>Activity#1: Interviews</h2>
+              <p>T you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc
+              </p>
+            </div>
+          </div>
+        </section>
 
-    </div>
-  </section>
-  
-  <section data-theme="green">
-    <div class="cha-header-clip" aria-hidden="true">
-      <header class="cha-header">
-        case study: ExpAR store
-      </header>
-    </div>
-    <div class="cha-text">
-      There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc
-    </div>
-  </section>
-</main>
+        <section data-theme="green">
+          <div class="cha-header-clip" aria-hidden="true">
+            <header class="cha-header">
+              case study: ExpAR store
+            </header>
+          </div>
+          <div class="cha-text">
+            There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc
+          </div>
+        </section>
+      </main>
     </div>
 </template>
 
 <script>
+//Intersection observer API//
+ //  const images = document.querySelectorAll('.anim');
 
+ //  observer = new IntersectionObserver((entries) => {
+ //    entries.forEach(entry => {
+ //      if(entry.intersectionRatio > 0) {
+//       entry.target.style.animation = `anim1 2s $(entry.target.dataset.delay} forwards ease-out`;
+ //      }
+ //      else {
+ //        entry.target.style.animation = 'none';
+ //      }
+//    })
+// })
+
+// images.forEach(image => {
+//  observer.observe(image)
+// })
 
 
 </script>
@@ -113,8 +128,8 @@ section {
 
     }
     .image {
-      width: 100%;
-      //opacity: 0;
+      width: 80%;
+     // opacity: 0;
     }
     .symbol {
       position:absolute;
@@ -132,6 +147,22 @@ section {
 
       }
     }
+
+.anim {
+  // opacity: 0;
+}
+
+@keyframes anim1 {
+  from {
+    transform: translateY(-100px);
+    opacity: 0;
+  }
+  to {
+    transform: translateY(0);
+    opacity: 1;
+  }
+ 
+}
 
 }
 
--- pages/index.vue
+++ pages/index.vue
@@ -68,10 +68,6 @@
 </template>
 
 <script>
-
-export default {
-
-}
 </script>
 
 <style>
