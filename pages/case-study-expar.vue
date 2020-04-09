e60450a668070d2d7009b81ac08ae50a51b1d5f0
MareK985 <59637752+MareK985@users.noreply.github.com>
2020-04-09 20:36:27 +0200

    Intersection observer setup

--- pages/case-study-expar.vue
+++ pages/case-study-expar.vue
@@ -8,20 +8,29 @@
       </header>
     </div>
     <div class="cha-text mt-2">
-      <Simple/>
-      <Stagger/>
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+      Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
     </div>
   </section>
   
-  <section data-theme="dark">
+  <section class="content" data-theme="dark">
     <div class="cha-header-clip" aria-hidden="true">
       <header class="cha-header">
         case study: ExpAR store
       </header>
     </div>
-    <div class="cha-text mt-2">
-      Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+    <div class="cha-text mt-2 left">
+      <img src="~/assets/img/path.jpg" width="400px" alt="image" class="image"/>
+      <p>The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+      </p>
+      <img src="~/assets/img/logo-1.svg" width="400px" alt="symbol" class="symbol"/>
+    </div>
+    <div class="cha-text mt-2 right">
+      <div>
+        <h2>Activity#1: Interviews</h2>
+        <p>T you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc
+    	  </p>
+      </div>
+
     </div>
   </section>
   
@@ -32,7 +41,6 @@
       </header>
     </div>
     <div class="cha-text">
-      <Simple/>
       There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc
     </div>
   </section>
@@ -42,17 +50,8 @@
 
 <script>
 
-import Simple from '~/components/Simple.vue'
-import Stagger from '~/components/Stagger.vue'
 
 
-export default {
-  components: {
-    Simple,
-    Stagger
-  }
-  
-}
 </script>
 
 
@@ -60,10 +59,13 @@ export default {
 @import url('https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900&display=swap');
 
 body {
+    height: 100vh;
     margin: 0;
     font-family: Lato;
 }
 section {
+  width: 80%;
+  height: 100vh;  //makes section full height!!!
   margin: 0 auto;
   position: relative;
   padding: 50px;
@@ -85,6 +87,52 @@ section {
       background: white;
     }
   }
+}
+
+.content {
+  display: grid;
+  grid-template-columns: 70% auto;
+
+    .left {
+      position: relative;
+      text-align: center;
+      display:grid;
+      place-items: center;
+
+      p {
+        background: white;
+        position: absolute;
+        margin-top: -35%;
+        right: 5%;
+        color: black;
+        padding: 1em;
+        border-radius: 15px;
+        width: 20vw;
+        text-align: left;
+      }
+
+    }
+    .image {
+      width: 100%;
+      //opacity: 0;
+    }
+    .symbol {
+      position:absolute;
+      margin-top: 32;
+      left: 19%;
+      width: 35vh;
+    }
+    .right {
+      display: grid;
+      place-items: center;
+
+      h2 {
+        color: white;
+        font-size: 1.7rem;
+
+      }
+    }
+
 }
 
 .cha-header-clip {
@@ -113,6 +161,8 @@ section {
   max-width: 700px;
   margin: 0 auto;
 }
+// group transitions//
+
 
 </style>
 
