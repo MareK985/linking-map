e89979ef2221cc29b78b4616b6911b56ec19f67e
MareK985 <59637752+MareK985@users.noreply.github.com>
2020-04-15 13:00:30 +0200

    Transparent menu header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,50 @@
 <template>
   <div id="app">
+    <div class="header">
+      <div class="nav">
+        <ul class=menu-bar>
+          <li>About us</li>
+          <li>Projects</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </div>
     <nuxt />
   </div>
 </template>
 
 
-
 <style>
+* {
+  padding: 0px;
+  margin: 0px;
+  box-sizing: border-box;
+}
+.header {
+  width: 100%;
+}
+.nav .menu-bar {
+  width: 100%;
+  text-align: center;
+  box-shadow: 1px 2px 4px grey;
+  background: transparent;
+  position: fixed;;
+
+}
+.menu-bar li {
+  list-style-type: none;
+  display: inline-block;
+  padding: 20px;
+  margin: 0px 30px;
+  font-size: 1.4em;
+  cursor: pointer;
+  font-weight: 400;
+  color: white;
+  border: none;
+  text-transform: uppercase;
+}
+.menu-bar li:hover {
+  background:rgba(125, 207, 182, 0.8);
+  color: white;
+}
     </style>
