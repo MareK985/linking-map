9e836ffa5810d16f2ac16371a24c2e2c7c97cf79
MareK985 <59637752+MareK985@users.noreply.github.com>
2020-04-23 10:50:31 +0200

    div.animation-hero

---
+++ components/CreativeDivBox.vue
@@ -0,0 +1,137 @@
+<template>
+    <div class="square">
+        <span></span>
+        <span></span>
+        <span></span>
+        <div class="content">
+            <h2 style="margin-bottom:0; font-size: 2.5em;">Leading sustainable solutions</h2>
+            <a href="/case-studies/expar-store" to="/case-studies/expar-store">OUR PROJECTS...</a>
+        </div>
+
+    </div>
+</template>
+
+
+<style scoped>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.square {
+    position: relative;
+    width: 400px;
+    height: 400px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+@media (max-width: 680px) {
+    .square {
+        width: 250px;
+        height: 250px;
+    }
+    .content {
+        font-size: 0.8em;
+        padding: 20px 30px;
+    }
+    .content a {
+        margin-top: 5px;
+        border: 1px solid white;
+        padding: 4px 12px;
+        color: white;
+        font-weight: 4;
+    }
+}
+.square span:nth-child(1) {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border: 2px solid white;
+    border-radius: 38% 62% 63% 37% / 41% 44% 56% 59%;
+    transition: 0.5s;
+    animation: animate 6s linear infinite;
+}
+.square:hover span:nth-child(1) {
+    background: rgba(125, 207, 182, 0.4);
+    border: none;
+}
+.square span:nth-child(2) {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border: 2px solid white;
+    border-radius: 38% 62% 63% 37% / 41% 44% 56% 59%;
+    transition: 0.5s;
+    animation: animate 4s linear infinite;
+}
+.square:hover span:nth-child(2) {
+    background: rgba(125, 207, 182, 0.4);
+    border: none;
+}
+.square span:nth-child(3) {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border: 2px solid white;
+    border-radius: 38% 62% 63% 37% / 41% 44% 56% 59%;
+    transition: 0.5s;
+    animation: animate2 10s linear infinite;
+}
+.square:hover span:nth-child(3) {
+    background: rgba(125, 207, 182, 0.4);
+    border: none;
+}
+@keyframes animate {
+    0% {
+        transform: rotate(0deg);
+    }
+    100% {
+        transform: rotate(360deg);
+    }
+}
+@keyframes animate2 {
+    0% {
+        transform: rotate(360deg);
+    }
+    100% {
+        transform: rotate(0deg);
+    }
+}
+.content {
+    position: relative;
+    padding: 40px 60px;
+    color: white;
+    text-align: center;
+    transition: 0.5s;
+    z-index: 1000;
+}
+.content a {
+    position: relative;
+    display: inline-block;
+    margin-top: 10px;
+    border: 2px solid white;
+    padding: 6px 18px;
+    text-decoration: none;
+    color: white;
+    font-weight: 600;
+    border-radius: 3%;
+}
+.content a:hover {
+    background: white;
+    color: #333;
+}
+
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -54,19 +54,28 @@
           </svg>
 
         </div>
-        <h2>Leading sustainable solutions</h2>
-        <a href="/case-studies/expar-store" to="/case-studies/expar-store" class="btn">OUR PROJECTS...</a>
+        <div class="div-box">
+          <CreativeDivBox/>
+        </div>
       </div>
     </section>
-    <main>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec nibh molestie, efficitur leo sed, viverra nunc. Donec vehicula accumsan erat facilisis ullamcorper. Donec commodo quis dui nec placerat. Donec mi orci, scelerisque eget nisl ac, hendrerit condimentum odio. Nam dictum odio eget quam tempus, a mattis odio ornare. Nullam auctor libero ut libero suscipit, ut accumsan nunc condimentum. Donec ullamcorper maximus sapien quis egestas.</p>
-      <p>Mauris viverra scelerisque lobortis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce ultrices enim sit amet elit tincidunt maximus. Suspendisse vitae pellentesque lectus. Duis commodo leo suscipit augue mollis, non venenatis dolor ullamcorper. Duis tincidunt scelerisque lacus, vel vehicula leo consectetur vel. Duis posuere nisl non odio consequat ultrices. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-      <p>Etiam a leo nec mi blandit euismod. Etiam fringilla odio vitae risus ornare, id bibendum velit consequat. Fusce posuere risus sollicitudin condimentum ultrices. Fusce gravida, purus eget laoreet mattis, velit sapien ultrices diam, id dapibus erat leo id quam. Maecenas quis risus convallis, placerat elit non, iaculis tortor. Nullam porttitor magna risus, quis bibendum metus tincidunt in. Etiam vel ligula ac risus mattis tincidunt vel sit amet ante. Morbi et viverra ligula. Ut ac dignissim nisi, condimentum imperdiet mauris. Pellentesque ut ipsum vel diam tristique faucibus eu et lectus. Maecenas posuere neque non lacus bibendum, sit amet pharetra justo semper. Sed mi risus, tempor sit amet ligula eget, varius pretium est. Sed a odio in orci accumsan pretium suscipit ut quam.</p>
-    </main>
+    <section class="intro">
+      <h1>Our service</h1>
+
+    </section>
   </div>
 </template>
 
 <script>
+
+import CreativeDivBox from '~/components/CreativeDivBox.vue'
+
+export default {
+  components: {
+    CreativeDivBox
+  }
+}
+
 </script>
 
 <style>
@@ -75,6 +84,7 @@
 
 body {
     margin: 0;
+    padding: 0;
     font-family: Lato;
 }
 .hero {
@@ -178,7 +188,9 @@ path {
     stroke-dashoffset: 0;
 
   }
-
+}
+.intro {
+  height: 100vh;
 }
 
 
