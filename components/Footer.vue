4a15855e180d0c3c082a73e9647830326c9178d0
MareK985 <59637752+MareK985@users.noreply.github.com>
2020-05-01 22:01:21 +0200

    responsive footer text

--- components/Footer.vue
+++ components/Footer.vue
@@ -4,7 +4,12 @@
          <div id="container">
                 <div id="cont">
                 <div class="footer_center">
-                    <h3>Classy footer text</h3>
+                    <h3 style="color: white; font-size: 1.8em; font-weight: 100; margin-top: 50px;">Hire us</h3>
+                    <div class="footer-text"> We are currently taking on new projects. Would you like to discuss yours?</div>
+                    <p style="color: white; font-size: 0.8rem; font-weight: 300; margin-left: 30px"> +386 40 899 409</p>
+                    <a href="https://www.linkedin.com/" target="_blank"><font-awesome-icon style="margin-left: 30px; margin-right: 10px;" :icon="['fab', 'linkedin-in']"/></a>
+                    <a href="mailto:info@linking-map.com" target="_blank"><font-awesome-icon :icon="['fas', 'envelope']"/></a>
+
                 </div>
             </div>
         </div>
@@ -56,7 +61,7 @@ section{
     left:0;
     position:fixed;
     width: 100%;
-    height: 2em;
+    height: 2.4em;
     overflow:hidden;
     margin:0 auto;
     -webkit-transition: all 1s ease;
@@ -77,16 +82,16 @@ section{
 .footer #container{
     margin-top:5px;
     width:100%;
-height:100%;
-  position:relative;
-  top:0;
-  left:0;
+    height:100%;
+    position:relative;
+    top:0;
+    left:0;
     background: rgb(125, 207, 182);
 }
 .footer #cont{
-  position:relative;
-  top:-45px;
-  right:190px;
+    position:relative;
+    top:-45px;
+    right:190px;
     width:150px;
     height:auto;
     margin:0 auto;
@@ -94,7 +99,7 @@ height:100%;
 .footer_center{
     width:500px;
     float:left;
-  text-align:center;
+    text-align:center;
 }
 .footer h3{
     font-family: 'Helvetica';
@@ -102,6 +107,27 @@ height:100%;
     font-weight: 100;
     margin-top:70px;
     margin-left:40px;
+}
+
+.footer-text {
+    color: white; 
+    font-size: 0.85rem; 
+    font-weight: 300; 
+    margin-left: 10px;
+}
+
+@media screen and (min-width: 601px) {
+.footer-text {
+    font-size: 0.9em;
+    margin-left: 30px;
+  }
+}
+
+@media screen and (max-width: 600px) {
+.footer-text {
+    font-size: 0.55em;
+    margin-left: 30px;
+  }
 }
 
 </style>
